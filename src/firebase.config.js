db7187829f3bc9eaf1b59528cfac53fcc94625f9
jaypals <jaypalis351@gmail.com>
2023-05-19 00:34:57 +0800

    temporary remove firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,15 +1,2 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-	apiKey: "AIzaSyAXqUf0jter6DYUBZtaFcSETSX0dvYGCFA",
-	authDomain: "house-marketplace-app-70bdf.firebaseapp.com",
-	projectId: "house-marketplace-app-70bdf",
-	storageBucket: "house-marketplace-app-70bdf.appspot.com",
-	messagingSenderId: "345351647206",
-	appId: "1:345351647206:web:fac985e14d0348d478b027",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
+// CONFIG IN FIREBASE
+// TEMPORARY EMPTY
