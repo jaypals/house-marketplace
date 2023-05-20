7ffd615d301f792d981f0e58babf4b9124573930
jaypals <jaypalis351@gmail.com>
2023-05-21 02:42:23 +0800

    added protected route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
-// import Profile from "./pages/Profile";
+import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -14,13 +17,16 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Explore />} />
 					<Route path='/offers' element={<Offers />} />
-					<Route path='/profile' element={<Signin />} />
+					<Route path='/profile' element={<PrivateRoute />}>
+						<Route path='/profile' element={<Profile />} />
+					</Route>
 					<Route path='/signin' element={<Signin />} />
 					<Route path='/signup' element={<Signup />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 				</Routes>
 				<Navbar />
 			</Router>
+			<ToastContainer />
 		</>
 	);
 }
